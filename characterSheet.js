0f244e3f49461d376508ed8c61349b184c25a39b
MMCALIXTO <88215430+MMCALIXTO@users.noreply.github.com>
2022-02-28 22:58:58 -0300

    Update characterSheet.js

--- characterSheet.js
+++ characterSheet.js
@@ -457,7 +457,7 @@ function addAttribute(attribute, id) {
       <img class="attributeDice" src="./img/dado.png" alt="Dado">
     </a>
     <h3>${attribute.type}</h3>
-    <input type="text" name="appearance" value="${attribute.amount}"  id="attribute_input_${id}" >
+    <input type="text" name="appearance" value="${attribute.amount}"  id="attribute_input_${id}" disabled >
   </div>
   
 <script>
@@ -630,16 +630,19 @@ function deleteWeapon(id) {
 
 //para vida
 
-
 function tirar(){
   
   let current = Number($('#lifeCurrent').val()) - 1
   const max = Number($('#lifeMax').val())
-
+  
   
   data.life.current = current
   data.life.max = max
 
+  const zero = 0
+  if (current < zero) {
+    current.add(++zero)
+  }
 
 
   $('.lifeBar').css('width', `${calculateBar(data.life.current, data.life.max)}%`)
@@ -659,7 +662,9 @@ function add(){
   data.life.current = current
   data.life.max = max
 
-
+if (current > max) {
+    current.add(--max)
+  }
 
   $('.lifeBar').css('width', `${calculateBar(data.life.current, data.life.max)}%`)
   
@@ -668,7 +673,6 @@ function add(){
   
 
 }
-
 
 
 
